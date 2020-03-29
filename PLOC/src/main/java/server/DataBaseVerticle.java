943fd487458a54e1470948a427804078cca77b93
Placix5 <placix5@gmail.com>
2020-03-29 17:59:23 +0200

    Conexión con la base de datos realizada

---
+++ PLOC/src/main/java/server/DataBaseVerticle.java
@@ -0,0 +1,60 @@
+package server;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.mysqlclient.MySQLConnectOptions;
+import io.vertx.mysqlclient.MySQLPool;
+import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import types.SensorProximidad;
+
+public class DataBaseVerticle extends AbstractVerticle{
+	
+	private MySQLPool dataBase;
+	
+	@Override
+	public void start(Promise<Void> startPromise) {
+		
+		MySQLConnectOptions connectOption = new MySQLConnectOptions().setPort(3306).setHost("localhost")
+				.setDatabase("proyecto_ploc").setUser("root").setPassword("contraseña");
+		PoolOptions poolOption = new PoolOptions().setMaxSize(5);
+		
+		dataBase = MySQLPool.pool(vertx, connectOption, poolOption); 
+		
+		Router router = Router.router(vertx);
+		vertx.createHttpServer().requestHandler(router::handle).listen(8085, result -> {
+			if(result.succeeded()) {
+				startPromise.complete();
+			}else {
+				startPromise.fail(result.cause());
+			}
+		});
+		router.get("/api/sensores/:idSensor").handler(this::getValueBySensorID);
+	}
+	
+	private void getValueBySensorID (RoutingContext context) {
+		
+		dataBase.query("SELECT * FROM proyecto_ploc.sensores_proximidad WHERE id_sensor_prox = " + context.request().getParam("idSensor"), res -> {
+			if(res.succeeded()) {
+				RowSet<Row> resultSet = res.result();
+				System.out.println("El número de elementos obtenidos es: " + resultSet.size());
+				
+				JsonArray result = new JsonArray();
+				for(Row r : resultSet) {
+					result.add(JsonObject.mapFrom(new SensorProximidad(r.getInteger("ID_SENSOR_PROX"),r.getLong("TIMESTAMP"),r.getBoolean("OBSTACULO"))));
+				}
+				context.response().setStatusCode(200).putHeader("content-type", "application/json").end(result.encodePrettily());
+			}else {
+				System.out.println("Algo salio mal :(");
+				context.response().setStatusCode(401).putHeader("content-type", "application/json").end(JsonObject.mapFrom(res.cause()).encodePrettily());
+			}
+		});
+		
+	}
+
+}
--- PLOC/src/main/java/server/MainServer.java
+++ PLOC/src/main/java/server/MainServer.java
@@ -1,27 +1,28 @@
 package server;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
+import io.vertx.core.Promise;
 
 public class MainServer extends AbstractVerticle{
 	
-	@SuppressWarnings("deprecation")
 	@Override
-	public void start(Future<Void> startFuture) {
+	public void start(Promise<Void> startPromise) {
 			
-		vertx.createHttpServer().requestHandler(r -> {
-			
-//			r.response().sendFile("prueba.html");
-			r.response().end("<h1>Bienvenido al servidor de pruebas</h1> Si esta viendo esto, es porque pertenece al proyecto PLOC, en caso"
-					+ "contrario, vuelvase por donde ha venido :D");
-			
-		}).listen(8085, result -> {
-			if(result.succeeded()) {
-				startFuture.complete();
-			}else {
-				startFuture.fail(result.cause());
-			}
-		});
+//		vertx.createHttpServer().requestHandler(r -> {
+//			
+////			r.response().sendFile("prueba.html");
+//			r.response().end("<h1>Bienvenido al servidor de pruebas</h1> Si esta viendo esto, es porque pertenece al proyecto PLOC, en caso"
+//					+ "contrario, vuelvase por donde ha venido :D");
+//			
+//		}).listen(8085, result -> {
+//			if(result.succeeded()) {
+//				startPromise.complete();
+//			}else {
+//				startPromise.fail(result.cause());
+//			}
+//		});
+		
+		vertx.deployVerticle(DataBaseVerticle.class.getName());
 		
 	}
 
