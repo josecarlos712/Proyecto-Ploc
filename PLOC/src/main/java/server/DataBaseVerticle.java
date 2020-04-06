457a4aab448a91758ba355c2399350adedd2e37b
Placix5 <placix5@gmail.com>
2020-04-06 13:10:09 +0200

    Terminada API REST con conexión a base de datos, con posible ampliación

--- PLOC/src/main/java/server/DataBaseVerticle.java
+++ PLOC/src/main/java/server/DataBaseVerticle.java
@@ -42,19 +42,23 @@ public class DataBaseVerticle extends AbstractVerticle{
 		//-----------------------------------------------------------------TABLA DRONES------------------------------------------------------------
 		
 		router.route("/api/drones").handler(BodyHandler.create());
+		router.route("/api/drones/*").handler(BodyHandler.create());
 		
 		router.get("/api/drones").handler(this::getAllDrones);
 		router.get("/api/drones/:idDron").handler(this::getDonByDronID);
 		router.post("/api/drones").handler(this::postDrone);
+		router.put("/api/drones/:idDron").handler(this::putDrone);
 		router.delete("/api/drones/:idDron").handler(this::deleteDronByDronID);
 		
 		//-----------------------------------------------------------------TABLA SENSORES------------------------------------------------------------
 		
 		router.route("/api/sensores").handler(BodyHandler.create());
+		router.route("/api/sensores/*").handler(BodyHandler.create());
 		
 		router.get("/api/sensores").handler(this::getAllSensors);
 		router.get("/api/sensores/:idSensor").handler(this::getSensorBySensorID);
 		router.post("/api/sensores").handler(this::postSensor);
+		router.put("/api/sensores/:idSensor").handler(this::putSensor);
 		router.delete("/api/sensores/:idSensor").handler(this::deleteSensorBySensorID);
 		
 		//-----------------------------------------------------------------TABLA VALORES------------------------------------------------------------
@@ -69,10 +73,12 @@ public class DataBaseVerticle extends AbstractVerticle{
 		//-----------------------------------------------------------------TABLA RUTAS------------------------------------------------------------
 				
 		router.route("/api/rutas").handler(BodyHandler.create());
+		router.route("/api/rutas/*").handler(BodyHandler.create());
 		
 		router.get("/api/rutas").handler(this::getAllPaths);
 		router.get("/api/rutas/:idRuta").handler(this::getPathByPathID);
 		router.post("/api/rutas").handler(this::postPath);
+		router.put("/api/rutas/:idRuta").handler(this::putPath);
 		router.delete("/api/rutas/:idRuta").handler(this::deletePathByPathID);
 	}
 	
@@ -120,19 +126,6 @@ public class DataBaseVerticle extends AbstractVerticle{
 		
 	}
 	
-	private void deleteDronByDronID(RoutingContext context) {
-		
-		dataBase.query("DELETE FROM proyecto_ploc.drones WHERE id_dron = " + context.request().getParam("idDron"), res -> {
-			if(res.succeeded()) {
-				System.out.println("Entrada borrada con éxito :D");
-				context.response().setStatusCode(201).putHeader("content-type", "application/json").end();
-			}else {
-				System.out.println("Algo salio mal :(\n");
-				context.response().setStatusCode(401).putHeader("content-type", "application/json").end(JsonObject.mapFrom(res.cause()).encodePrettily());
-			}
-		});
-	}
-	
 	private void postDrone(RoutingContext context) {		
 		JsonObject body = context.getBodyAsJson();
 		
@@ -151,6 +144,41 @@ public class DataBaseVerticle extends AbstractVerticle{
 		});
 		
 	}
+	
+	private void putDrone(RoutingContext context) {
+		
+		JsonObject body = context.getBodyAsJson();
+		
+		dataBase.query("UPDATE proyecto_ploc.drones SET ID_DRON = " + body.getInteger("idDron") + ", PESO_SOPORTADO = " + body.getInteger("pesoSoportado") + 
+				", BATERIA = " + body.getInteger("bateria") + ", ESTADO = '" + body.getString("estado") + "', PARKING_PATH = '" + body.getString("parkingPath") + 
+				"', ID_SENSOR = " + body.getInteger("idSensor") + ", ID_RUTA = " + body.getInteger("idRuta") + " WHERE ID_DRON = " + context.request().getParam("idDron")
+				, res -> {
+			
+			if(res.succeeded()) {
+				System.out.println("Se ha actualizado con exito :D\n");
+				context.response().setStatusCode(201).putHeader("content-type", "application/json").end(body.encodePrettily());
+			}else {
+				System.out.println("Algo ha salido mal :(");
+				context.response().setStatusCode(401).putHeader("content-type", "application/json").end(JsonObject.mapFrom(res.cause()).encodePrettily());
+			}
+			
+		});
+		
+	}
+	
+	private void deleteDronByDronID(RoutingContext context) {
+		
+		dataBase.query("DELETE FROM proyecto_ploc.drones WHERE id_dron = " + context.request().getParam("idDron"), res -> {
+			if(res.succeeded()) {
+				System.out.println("Entrada borrada con éxito :D");
+				context.response().setStatusCode(201).putHeader("content-type", "application/json").end();
+			}else {
+				System.out.println("Algo salio mal :(\n");
+				context.response().setStatusCode(401).putHeader("content-type", "application/json").end(JsonObject.mapFrom(res.cause()).encodePrettily());
+			}
+		});
+	}
+
 	
 	//-----------------------------------------------------------------TABLA SENSORES------------------------------------------------------------
 	
@@ -194,20 +222,6 @@ public class DataBaseVerticle extends AbstractVerticle{
 		
 	}
 	
-	private void deleteSensorBySensorID(RoutingContext context) {
-
-		
-		dataBase.query("DELETE FROM proyecto_ploc.sensores WHERE id_sensor = " + context.request().getParam("idSensor"), res -> {
-			if(res.succeeded()) {
-				System.out.println("Entrada borrada con éxito :D");
-				context.response().setStatusCode(201).putHeader("content-type", "application/json").end();
-			}else {
-				System.out.println("Algo salio mal :(\n");
-				context.response().setStatusCode(401).putHeader("content-type", "application/json").end(JsonObject.mapFrom(res.cause()).encodePrettily());
-			}
-		});
-	}
-	
 	private void postSensor(RoutingContext context) {		
 		JsonObject body = context.getBodyAsJson();
 		
@@ -225,6 +239,41 @@ public class DataBaseVerticle extends AbstractVerticle{
 		});
 		
 	}
+	
+	private void putSensor(RoutingContext context) {
+		
+		JsonObject body = context.getBodyAsJson();
+		
+		dataBase.query("UPDATE proyecto_ploc.sensores SET ID_SENSOR = " + body.getInteger("idSensor") + ", TIPO = '" + body.getString("tipo") + 
+				"', TIEMPO_ACT = " + body.getInteger("tiempoAct") + " WHERE ID_SENSOR = " + context.request().getParam("idSensor")
+				, res -> {
+			
+			if(res.succeeded()) {
+				System.out.println("Se ha actualizado con exito :D\n");
+				context.response().setStatusCode(201).putHeader("content-type", "application/json").end(body.encodePrettily());
+			}else {
+				System.out.println("Algo ha salido mal :(");
+				context.response().setStatusCode(401).putHeader("content-type", "application/json").end(JsonObject.mapFrom(res.cause()).encodePrettily());
+			}
+			
+		});
+		
+	}
+	
+	private void deleteSensorBySensorID(RoutingContext context) {
+
+		
+		dataBase.query("DELETE FROM proyecto_ploc.sensores WHERE id_sensor = " + context.request().getParam("idSensor"), res -> {
+			if(res.succeeded()) {
+				System.out.println("Entrada borrada con éxito :D");
+				context.response().setStatusCode(201).putHeader("content-type", "application/json").end();
+			}else {
+				System.out.println("Algo salio mal :(\n");
+				context.response().setStatusCode(401).putHeader("content-type", "application/json").end(JsonObject.mapFrom(res.cause()).encodePrettily());
+			}
+		});
+	}
+
 	
 	//-----------------------------------------------------------------TABLA VALORES------------------------------------------------------------
 
@@ -349,6 +398,26 @@ public class DataBaseVerticle extends AbstractVerticle{
 			
 			if(res.succeeded()) {
 				System.out.println("Se ha añadido la línea con éxito :D");
+				context.response().setStatusCode(201).putHeader("content-type", "application/json").end(body.encodePrettily());
+			}else {
+				System.out.println("Algo ha salido mal :(");
+				context.response().setStatusCode(401).putHeader("content-type", "application/json").end(JsonObject.mapFrom(res.cause()).encodePrettily());
+			}
+			
+		});
+		
+	}
+	
+	private void putPath(RoutingContext context) {
+		
+		JsonObject body = context.getBodyAsJson();
+		
+		dataBase.query("UPDATE proyecto_ploc.rutas SET ID_RUTA = " + body.getInteger("idRuta") + ", PATH = '" + body.getString("path") + 
+				"', TIEMPO_RUTA = " + body.getLong("tiempoRuta") + " WHERE ID_RUTA = " + context.request().getParam("idRuta")
+				, res -> {
+			
+			if(res.succeeded()) {
+				System.out.println("Se ha actualizado con exito :D\n");
 				context.response().setStatusCode(201).putHeader("content-type", "application/json").end(body.encodePrettily());
 			}else {
 				System.out.println("Algo ha salido mal :(");
